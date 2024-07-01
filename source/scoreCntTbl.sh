45c69f3dec29858791c43d5e3f1aa4701a037a90
Harshit Ghosh <hg2604@g160.cm.cluster>
2024-07-01 14:53:50 -0400

    Fixed Scoring

--- source/scoreCntTbl.sh
+++ source/scoreCntTbl.sh
@@ -12,7 +12,7 @@ echo $filename
 awk '{print $1}' $count_table > $base_dr/$filename.txt
 
 #Score the seq.txt file using ProboundTools
-java -cp /burg/home/hg2604/bin/ProBoundTools/ProBoundTools/target/ProBound-jar-with-dependencies.jar  proBoundTools/App -c "loadMotifCentralModel($fit_id).addNScoring().inputTXT($base_dr/$filename.txt).bindingModeScores(/dev/stdout)" | awk '{print $2}' >  ${base_dr}/${filename}_${fit_id}.txt
+java -cp /burg/home/hg2604/bin/ProBoundTools/ProBoundTools/target/ProBound-jar-with-dependencies.jar  proBoundTools/App -c "loadMotifCentralModel($fit_id).buildConsensusModel().addNScoring().inputTXT($base_dr/$filename.txt).bindingModeScores(/dev/stdout)" | awk '{print $2}' >  ${base_dr}/${filename}_${fit_id}.txt
 
 #Deleting the temp files
 rm $base_dr/$filename.txt
